d09159c912dc10a0565c54f32f1a64631484975c
mason <masonbot19@gmail.com>
2022-11-08 20:20:55 -0800

    test

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,9 +8,9 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
-    for (int i=1; i<N; i++) {
-        for (int j=1; j<N; k++) {
-            y[i] = y[i] + A[i][j] * x[j];
+    for (int i=1; i<n; i++) {
+        for (int j=1; j<n; j++) {
+            y[i] = y[i] + A[i]*A[j] * x[j];
         }
     }
 }
