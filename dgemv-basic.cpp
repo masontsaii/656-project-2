514f084ba073226fae9fde2df552a456f5b7c57b
mason <masonbot19@gmail.com>
2022-11-08 20:47:27 -0800

    push back to original

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -6,7 +6,7 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double** A, double* x, double* y) {
+void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
     for (int i=1; i<n; i++) {
         for (int j=1; j<n; j++) {
